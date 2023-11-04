f93e390214b208c4e0204306e42f23bbf60408c7
BelmontR <47500226+BelmontR@users.noreply.github.com>
2023-11-04 17:14:22 +0100

    Scaleing and Equiooing Effects

--- Franken GameJam 2023/Assets/InventoryManager.cs
+++ Franken GameJam 2023/Assets/InventoryManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class InventoryManager : MonoBehaviour
 {
@@ -9,6 +10,11 @@ public class InventoryManager : MonoBehaviour
     public ItemAmountMapper[] inventory;
     public int maxItemsCum;
     public int currentCum;
+
+    public DamageHitbox axe;
+    public DamageHitbox spear;
+    public DamageHitbox hammer;
+    public SlingshotController ssc;
 
     // Start is called before the first frame update
     void Awake()
@@ -51,11 +57,57 @@ public class InventoryManager : MonoBehaviour
 
     public void ActivateEffect(InventoryEntity item)
     {
+        switch (item.name)
+        {
+            case "Axe":
+                GameManager.instance.player.axeController.SetActive(true);
+                break;
 
+            case "Spear":
+                GameManager.instance.player.spearController.SetActive(true);
+                break;
+
+            case "Stone":
+                GameManager.instance.player.slingShotController.SetActive(true);
+                break;
+
+            case "Hammer":
+                GameManager.instance.player.clubController.SetActive(true);
+                break;
+
+            case "Bone":
+                break;
+
+            case "Egg":
+                break;
+
+        }
     }
 
     public void ScaleEffect(ItemAmountMapper iam)
     {
+        switch (iam.item.name)
+        {
+            case "Axe":
+                axe.stats.IncreaseKbStrength(iam.amount * 0.1f);
+                axe.stats.IncreaseKbLength(iam.amount * 0.05f);
+                axe.stats.IncreaseStrength(iam.amount * 0.05f);
+                break;
+
+            case "Spear":
+                axe.stats.IncreaseStrength(iam.amount * 0.2f);
+                break;
+
+            case "Stone":
+                axe.stats.IncreaseKbStrength(iam.amount * 0.05f);
+                axe.stats.IncreaseStrength(iam.amount * 0.025f);
+                break;
+
+            case "Hammer":
+                axe.stats.IncreaseKbStrength(iam.amount * 0.135f);
+                axe.stats.IncreaseKbLength(iam.amount * 0.075f);
+                break;
+        }
 
     }
 
--- Franken GameJam 2023/Assets/Scripts/Weapons/DamageHitbox.cs
+++ Franken GameJam 2023/Assets/Scripts/Weapons/DamageHitbox.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 
 public class DamageHitbox : MonoBehaviour
@@ -8,13 +9,59 @@ public class DamageHitbox : MonoBehaviour
     public float kbStrength;
     public float kbLength;
 
+    public dmgHbStats stats;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var enemy = collision.GetComponent<Enemy>();
         if(enemy != null)
         {
-            enemy.TakeHit(strength);
-            enemy.TakeKnockback(this.gameObject, kbStrength, kbLength);
+            enemy.TakeHit(stats.strength);
+            enemy.TakeKnockback(this.gameObject, stats.kbStrength, stats.kbLength);
+        }
+    }
+
+    public class dmgHbStats
+    {
+        public float strength;
+        public float kbStrength;
+        public float kbLength;
+
+        public dmgHbStats(float strength, float kbStrength, float kbLength)
+        {
+            this.strength = strength;
+            this.kbStrength = kbStrength;
+            this.kbLength = kbLength;
+        }
+
+        public void IncreaseStrength(float multiplicator)
+        {
+            strength *= multiplicator;
+        }
+
+        public void IncreaseKbStrength(float multiplicator)
+        {
+            kbStrength *= multiplicator;
+        }
+
+        public void IncreaseKbLength(float multiplicator)
+        {
+            kbStrength *= multiplicator;
+        }
+
+        public void SetStrength(float str)
+        {
+            strength = str;
+        }
+
+        public void SetKbStrength(float kbstr)
+        {
+            kbStrength = kbstr;
+        }
+
+        public void SetKbLength(float kblgt)
+        {
+            kbLength = kblgt;
         }
     }
 }
